6f51b154e4cd170920b0d509cd98ea21ff04c741
vlex127 <iwunoayokunle@gmail.com>
2025-11-08 02:58:33 +0100

    shiiiiiiiiiiiiii

--- src/app/book/[id]/page.tsx
+++ src/app/book/[id]/page.tsx
@@ -31,9 +31,7 @@ type Section = {
 
 // Add touch detection
 const isTouchDevice = () => {
-  return (('ontouchstart' in window) ||
-    (navigator.maxTouchPoints > 0) ||
-    (navigator.msMaxTouchPoints > 0));
+  return 'ontouchstart' in window || navigator.maxTouchPoints > 0;
 };
 
 export default function SeatSelection() {
